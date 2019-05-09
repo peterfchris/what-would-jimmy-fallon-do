8633ae38b4a00551edc5131cf6e1c129bee8c1d2
Peter Forsgren Christensen <peter.f.chris@gmail.com>
2019-05-09 10:35:44 -0600

    Components Added, AddQuestion finished

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import AddQuestion from './Components/AddQuestion'
+import AllQuestions from './Components/AllQuestions'
+import DeleteQuestion from './Components/DeleteQuestion'
 
 function App() {
   return (
     <div className="App">
-      
+      <AddQuestion />
+      <AllQuestions />
+      <DeleteQuestion />
     </div>
   );
 }
---
+++ src/Components/AddQuestion.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react'
+import axios from 'axios'
+
+class AddQuestion extends Component {
+    constructor(){
+        super()
+        this.state = {
+            question: '',
+            answer: null  //remember to come back and fix this, it should be randomly generated
+        }
+    }
+
+    handleUpdateInput = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value //figure out what this line does
+        })
+    }
+
+    handleAddEvent = (e) => {
+        e.preventDefault()
+        axios.post('/api/AddQuestion', {
+            name: this.state.question,
+            answer: this.state.answer
+        })
+    }
+
+    render(){
+        return(
+            <div>
+                <h1>Asky Jimmy Anything</h1>
+                <form onSubmit={this.handleAddEvent}>
+                    <indput
+                        placeholder="As long as it's PG rated..."
+                        name="question"
+                        onChange={this.handleUpdateInput} />
+                    <button>Ask Away!</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default AddQuestion
