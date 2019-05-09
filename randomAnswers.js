91357de13f9595cd230ff700f6356fd9babfa0b2
Peter Forsgren Christensen <peter.f.chris@gmail.com>
2019-05-09 13:50:49 -0600

    random answers added

---
+++ randomAnswers.js
@@ -0,0 +1,10 @@
+const randomAnswers = [
+    {id: 1, a: 'I like to settle these sorts of issues with a good old fashioned game of catchphrase'},
+    {id: 2, a: 'Never eat mayonaise.  It\'s disgusting.'},
+    {id: 3, a: 'Yes'},
+    {id: 4, a: 'No, that\'s a bad idea.'},
+    {id: 5, a: 'When in doubt, annoy your mentors.'},
+    {id: 6, a: 'Maybe'},
+    {id: 7, a: 'I wouldn\'t say no.'},
+    {id: 8, a: 'If you have to'}
+]
